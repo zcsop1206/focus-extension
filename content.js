645c5d3b2751eaf61c4cba92d933d52ccd1ab0ef
zcsop1206 <115398989+zcsop1206@users.noreply.github.com>
2025-03-23 16:24:50 +0000

    fixed webgazer integration

--- content.js
+++ content.js
@@ -1,60 +1,54 @@
-const webGazerScript = document.createElement('script');
-webGazerScript.src = "https://webgazer.cs.brown.edu/webgazer.js";
-document.head.appendChild(webGazerScript);
+// Import WebGazer.js from local resources
+importScripts(chrome.runtime.getURL("libs/webgazer.js"));
 
-webGazerScript.onload = async function () {
-  console.log("WebGazer.js loaded successfully.");
+webgazer.setGazeListener((data, timestamp) => {
+  if (data) {
+    handleGaze(data);
+  }
+}).begin();
 
-  // Start WebGazer
-  await webgazer.setGazeListener((data, timestamp) => {
-    if (data) {
-      handleGaze(data);
+console.log("WebGazer started.");
+webgazer.showVideoPreview(true).showPredictionPoints(true);
+
+let lastFocusedElement = null;
+let lostFocusTimeout = null;
+
+function handleGaze(data) {
+  const x = data.x;
+  const y = data.y;
+  const element = document.elementFromPoint(x, y);
+
+  if (element && (element.tagName === "P" || element.tagName === "SPAN" || element.tagName === "DIV")) {
+    if (lastFocusedElement !== element) {
+      console.log("Focused on new element:", element);
+      lastFocusedElement = element;
+
+      if (lostFocusTimeout) {
+        clearTimeout(lostFocusTimeout);
+        lostFocusTimeout = null;
+      }
     }
-  }).begin();
-
-  console.log("WebGazer started.");
-  webgazer.showVideoPreview(true).showPredictionPoints(true);
-
-  let lastFocusedElement = null;
-  let lostFocusTimeout = null;
-
-  function handleGaze(data) {
-    const x = data.x;
-    const y = data.y;
-    const element = document.elementFromPoint(x, y);
-
-    if (element && (element.tagName === "P" || element.tagName === "SPAN" || element.tagName === "DIV")) {
-      if (lastFocusedElement !== element) {
-        console.log("Focused on new element:", element);
-        lastFocusedElement = element;
-
-        if (lostFocusTimeout) {
-          clearTimeout(lostFocusTimeout);
-          lostFocusTimeout = null;
+  } else {
+    if (!lostFocusTimeout) {
+      lostFocusTimeout = setTimeout(() => {
+        console.log("User lost focus. Applying bionic reading...");
+        if (lastFocusedElement) {
+          applyBionicReading(lastFocusedElement);
         }
-      }
-    } else {
-      if (!lostFocusTimeout) {
-        lostFocusTimeout = setTimeout(() => {
-          console.log("User lost focus. Applying bionic reading...");
-          if (lastFocusedElement) {
-            applyBionicReading(lastFocusedElement);
-          }
-        }, 2000);
-      }
+      }, 2000);
     }
   }
+}
 
-  function applyBionicReading(element) {
-    console.log("Applying bionic reading to:", element);
-    element.innerHTML = element.innerHTML.replace(/\b(\w{2,})/g, "<b style='color: red;'>$1</b>");
-  }
+function applyBionicReading(element) {
+  console.log("Applying bionic reading to:", element);
+  element.innerHTML = element.innerHTML.replace(/\b(\w{2,})/g, "<b style='color: red;'>$1</b>");
+}
 
-  function resetText(element) {
-    console.log("Resetting text for:", element);
-    element.innerHTML = element.innerHTML.replace(/<b style='color: red;'>(.*?)<\/b>/g, "$1");
-  }
-};
+function resetText(element) {
+  console.log("Resetting text for:", element);
+  element.innerHTML = element.innerHTML.replace(/<b style='color: red;'>(.*?)<\/b>/g, "$1");
+}
 
 // Listen for messages from popup or background scripts
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
