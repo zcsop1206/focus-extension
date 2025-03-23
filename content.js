4e2c377e5a59d695ac7057cd0ad0b6f53a9965e4
zcsop1206 <115398989+zcsop1206@users.noreply.github.com>
2025-03-23 16:43:46 +0000

    fixed errors

--- content.js
+++ content.js
@@ -8,67 +8,78 @@ if (!window.webGazerInjected) {
     webGazerScript.onload = () => {
       console.log("WebGazer.js loaded successfully.");
   
-      // Start WebGazer
-      webgazer.setGazeListener((data, timestamp) => {
-        if (data) {
-          handleGaze(data);
-        }
-      }).begin();
-  
-      console.log("WebGazer started.");
-      webgazer.showVideoPreview(true).showPredictionPoints(true);
-  
-      let lastFocusedElement = null;
-      let lostFocusTimeout = null;
-  
-      function handleGaze(data) {
-        const x = data.x;
-        const y = data.y;
-        const element = document.elementFromPoint(x, y);
-  
-        if (element && (element.tagName === "P" || element.tagName === "SPAN" || element.tagName === "DIV")) {
-          if (lastFocusedElement !== element) {
-            console.log("Focused on new element:", element);
-            lastFocusedElement = element;
-  
-            if (lostFocusTimeout) {
-              clearTimeout(lostFocusTimeout);
-              lostFocusTimeout = null;
-            }
+      // Inject a script to initialize WebGazer in the page context
+      const initScript = document.createElement('script');
+      initScript.textContent = `
+        window.webgazer.setGazeListener((data, timestamp) => {
+          if (data) {
+            window.dispatchEvent(new CustomEvent('webgazer-data', { detail: data }));
           }
-        } else {
-          if (!lostFocusTimeout) {
-            lostFocusTimeout = setTimeout(() => {
-              console.log("User lost focus. Applying bionic reading...");
-              if (lastFocusedElement) {
-                applyBionicReading(lastFocusedElement);
-              }
-            }, 2000);
-          }
-        }
-      }
-  
-      function applyBionicReading(element) {
-        console.log("Applying bionic reading to:", element);
-        element.innerHTML = element.innerHTML.replace(/\b(\w{2,})/g, "<b style='color: red;'>$1</b>");
-      }
-  
-      function resetText(element) {
-        console.log("Resetting text for:", element);
-        element.innerHTML = element.innerHTML.replace(/<b style='color: red;'>(.*?)<\/b>/g, "$1");
-      }
+        }).begin();
+        console.log("WebGazer started in page context.");
+        window.webgazer.showVideoPreview(true).showPredictionPoints(true);
+      `;
+      document.head.appendChild(initScript);
     };
   
     document.head.appendChild(webGazerScript);
   
+    let lastFocusedElement = null;
+    let lostFocusTimeout = null;
+  
+    // Listen for gaze data from the page context
+    window.addEventListener('webgazer-data', (event) => {
+      const data = event.detail;
+      handleGaze(data);
+    });
+  
+    function handleGaze(data) {
+      const x = data.x;
+      const y = data.y;
+      const element = document.elementFromPoint(x, y);
+  
+      if (element && (element.tagName === "P" || element.tagName === "SPAN" || element.tagName === "DIV")) {
+        if (lastFocusedElement !== element) {
+          console.log("Focused on new element:", element);
+          lastFocusedElement = element;
+  
+          if (lostFocusTimeout) {
+            clearTimeout(lostFocusTimeout);
+            lostFocusTimeout = null;
+          }
+        }
+      } else {
+        if (!lostFocusTimeout) {
+          lostFocusTimeout = setTimeout(() => {
+            console.log("User lost focus. Applying bionic reading...");
+            if (lastFocusedElement) {
+              applyBionicReading(lastFocusedElement);
+            }
+          }, 2000);
+        }
+      }
+    }
+  
+    function applyBionicReading(element) {
+      console.log("Applying bionic reading to:", element);
+      element.innerHTML = element.innerHTML.replace(/\b(\w{2,})/g, "<b style='color: red;'>$1</b>");
+    }
+  
+    function resetText(element) {
+      console.log("Resetting text for:", element);
+      element.innerHTML = element.innerHTML.replace(/<b style='color: red;'>(.*?)<\/b>/g, "$1");
+    }
+  
     // Listen for messages from popup or background scripts
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       if (message.action === "startWebGazer") {
-        webgazer.begin();
-        console.log("WebGazer started.");
+        const startScript = document.createElement('script');
+        startScript.textContent = `window.webgazer.begin(); console.log("WebGazer started.");`;
+        document.head.appendChild(startScript);
       } else if (message.action === "stopWebGazer") {
-        webgazer.end();
-        console.log("WebGazer stopped.");
+        const stopScript = document.createElement('script');
+        stopScript.textContent = `window.webgazer.end(); console.log("WebGazer stopped.");`;
+        document.head.appendChild(stopScript);
       }
     });
   }
